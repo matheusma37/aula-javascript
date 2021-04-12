092eaa97eae50ebffc8bfc203c08377f948d9ede
matheusma37 <mtsma37@gmail.com>
2021-04-11 21:23:32 -0300

    Finaliza o primeiro vídeo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = 'Matheus Machado';
+var n1 = 5;
+var n2 = 3;
+var frase = 'Senegal é a melhor seleção do mundo';
+// alert(nome + " tem " + idade + " anos");
+// alert(idade + idade2)
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+alert(frase.replace('Senegal', 'Brasil'))
