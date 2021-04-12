30fd06291e54677cb14a925c5c2227d0647c61c2
matheusma37 <mtsma37@gmail.com>
2021-04-11 23:40:21 -0300

    Finaliza o quinto vídeo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
-function validarIdade(idade) {
-  var validar;
-  if (idade >= 18) {
-    validar = true;
-  } else {
-    validar = false;
-  }
-  return validar;
+function clicou() {
+  document.getElementById('agradecimento').innerHTML = "<b>Obrigado por clicar</b>";
+  // console.log(document.getElementById('agradecimento'));
+  // alert("Obrigado por clicar")
 }
 
-var idade = prompt("Qual sua idade?");
-console.log(validarIdade(idade));
+function redirecionar() {
+  // window.open("https://web.digitalinnovation.one/");
+  window.location.href = "https://web.digitalinnovation.one/";
+}
+
+function trocar(elemento) {
+  elemento.innerHTML = "Obrigado por passar o mouse";
+  // document.getElementById('mousemove').innerHTML = 'Obrigado por passar o mouse';
+  // alert("trocar texto");
+}
+
+function voltar(elemento) {
+  elemento.innerHTML = "Passe o mouse aqui";
+  // document.getElementById('mousemove').innerHTML = 'Passe o mouse aqui';
+}
+
+function load() {
+  alert('página carregada');
+}
+
+function funcaoChange(elemento) {
+  console.log(elemento.value);
+}
+
+// function validarIdade(idade) {
+//   var validar;
+//   if (idade >= 18) {
+//     validar = true;
+//   } else {
+//     validar = false;
+//   }
+//   return validar;
+// }
+
+// var idade = prompt("Qual sua idade?");
+// console.log(validarIdade(idade));
 
 // function setReplace(frase, nome, novo_nome) {
 //   return frase.replace(nome, novo_nome);
