5d5e6e89c42e46b2d92a1c11afb1fe3278892f40
matheusma37 <mtsma37@gmail.com>
2021-04-11 21:33:52 -0300

    Finaliza o segundo vídeo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-var nome = 'Matheus Machado';
-var n1 = 5;
-var n2 = 3;
-var frase = 'Senegal é a melhor seleção do mundo';
+var frutas = [{ nome: 'maçã', cor: 'vermelha' }, { nome: 'uva', cor: 'roxa' }];
+console.log(frutas);
+alert(frutas[1].nome);
+
+// var fruta = { nome: 'maçã', cor: 'vermelha' };
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var lista = ['maçã', 'pêra', 'laranja'];
+// lista.push('uva');
+// lista.pop();
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(' - '));
+
+// var nome = 'Matheus Machado';
+// var n1 = 5;
+// var n2 = 3;
+// var frase = 'Senegal é a melhor seleção do mundo';
 // alert(nome + " tem " + idade + " anos");
 // alert(idade + idade2)
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.toLowerCase());
-alert(frase.replace('Senegal', 'Brasil'))
+// console.log(nome);
+// console.log(n1 * n2);
+// console.log(frase.toLowerCase());
+// alert(frase.replace('Senegal', 'Brasil'))
