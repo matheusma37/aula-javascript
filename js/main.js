f2b28317ff5fbc0208c6c6154a155e34a485860b
matheusma37 <mtsma37@gmail.com>
2021-04-11 21:53:04 -0300

    Finaliza o terceiro vídeo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-var frutas = [{ nome: 'maçã', cor: 'vermelha' }, { nome: 'uva', cor: 'roxa' }];
-console.log(frutas);
-alert(frutas[1].nome);
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+// var count;
+// for (count = 0; count <= 5; count++){
+//   alert(count);
+// }
+
+// var count = 0;
+// while (count <= 5) {
+//   console.log(count);
+//   alert(count);
+//   count++;
+// }
+
+// var idade = prompt('Qual sua idade?');
+// if (idade >= 18) {
+//   alert("Maior de idade");
+// } else {
+//   alert("Menor de idade");
+// }
+
+// var frutas = [{ nome: 'maçã', cor: 'vermelha' }, { nome: 'uva', cor: 'roxa' }];
+// console.log(frutas);
+// alert(frutas[1].nome);
 
 // var fruta = { nome: 'maçã', cor: 'vermelha' };
 // console.log(fruta.nome);
